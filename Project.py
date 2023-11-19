ecee9ba482796c67bcd32638a8bdebefe704ad1c
ShashankaShekharSharma <135298952+ShashankaShekharSharma@users.noreply.github.com>
2023-11-19 18:32:14 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,28 +1,36 @@
+
 '''
-# Chatbot Implementation
+Chatbot Implementation
 
 This code represents a sophisticated implementation of a chatbot that combines pattern matching techniques akin to ELIZA with modern features, such as sentiment analysis.
-It engages users in text-based conversations while providing emotional insights.
-It harnesses the TextBlob library to gauge sentiment in user inputs, fostering self-reflection and sharing of feelings.
-The code is designed for future enhancement through data analysis, particularly the content stored in CSV files (feedback_data and conversation_data).
-This accumulation of user interactions is essential for refining the chatbot's responses and making it more personalized.
-Furthermore, the code includes mechanisms to keep records, timestamps, and even invites user feedback, ensuring that it evolves to cater to the users' needs.
+It engages users in text-based conversations while providing emotional insights. The code can also perform calculations, show calendar, manage a to-do list and also display calendar. It also shows date and time when asked.
+It harnesses the TextBlob library to gauge sentiment in user inputs, fostering self-reflection and sharing of feelings. 
+The code is designed for future enhancement through data analysis, particularly the content stored in CSV files (feedback_data and conversation_data). 
+This accumulation of user interactions is essential for refining the chatbot's responses and making it more personalized. 
+Furthermore, the code includes mechanisms to keep records, timestamps, and even invites user feedback, ensuring that it evolves to cater to the users' needs. 
 The professional and responsive nature of this code signifies a powerful tool for simulating human-like interactions with users, paving the way for potential advancements in the field of conversational AI. The code encompasses elements such as date and time retrieval, user data logging, sentiment analysis, and user feedback integration, offering a comprehensive conversational experience.
 
-## Key Features
+Key Features
 - Pattern-based responses to user input
 - Sentiment analysis of user's emotional state
 - Date and time retrieval
 - User feedback collection
 - Conversation history recording
+- To do list
+- Calculator
+- Calendar
 
-### Usage:
+Usage:
 - Input "bye" to exit the conversation.
 - Input "date and time" to get the current date and time.
+- Input "calendar" to get calendar
+- Input "todo" to start todo list
+- Input "programmer's data" to get info related to programmer
+- Input "calendar" displays calendar of given year
 - The program saves user prompts for feedback and analysis.
 
-Developed By<br>
-Shashanka Shekhar Sharma<br>
+Developed By
+Shashanka Shekhar Sharma
 12.10.2023
 '''
 # Importing all Necessary Libraries
@@ -795,8 +803,10 @@ EEEEEEE     LLLLLLLLL IIIIIIII  ZZZZZZ  AA    AA''')
 # Introduction
 print("Welcome. I am Eliza. A Rogerian psychotherapist")
 print("Your prompts will be saved in order to make improvements in the code")
-print("Type Calculator to open Calculator")
 print("Type date and time to get date and time")
+print("Type todo list for opening to-do")
+print("Type Calendar to see calendar")
+print("Type calculator to open calculator")
 print("Type bye to exit")
 # Logical Part of Eliza
 # Includes conversation counter, history note maker, sentiment analysis, feedback receiver and date and time
